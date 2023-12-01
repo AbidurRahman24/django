57129d9042584f4e00181a719fe836ee37209365
Abidur Niloy <niloyabidur@gmail.com>
2023-12-01 19:25:48 +0600

    new apps

--- Pratices/practice 01/navigation 2/nav/views.py
+++ Pratices/practice 01/navigation 2/nav/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request, 'nav/home.html')
+def about(request):
+    return render(request, 'nav/about.html')
 
 def contact(request):
     return render(request, 'nav/contact.html')
