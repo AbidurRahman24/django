16c95198bfd1a310c5c5bfee0518476340c297ff
Abidur Niloy <niloyabidur@gmail.com>
2023-12-08 22:31:47 +0600

    week 4 assignment done

--- Exam/Assignment 02/task_management_system/task/urls.py
+++ Exam/Assignment 02/task_management_system/task/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('add/', views.add_task, name='add_task'),
     path('show/', views.show_task, name='show_task'),
+    path('', views.show_task, name='show_task'),
     path('edit/<int:id>', views.edit_task, name='edit_task'),
     path('delete/<int:id>', views.delete_task, name='delete_task'),
 ]
--- Exam/Assignment 02/task_management_system/task_management_system/urls.py
+++ Exam/Assignment 02/task_management_system/task_management_system/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='homepage'),
+    path('', include('task.urls')),
     path('task/', include('task.urls')),
     path('category/', include('category.urls')),
 ]
