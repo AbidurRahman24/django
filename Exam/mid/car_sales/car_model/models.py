19be1e39ac84843e17ee05343d874514545ba6db
Abidur Niloy <niloyabidur@gmail.com>
2023-12-22 17:05:07 +0600

    buy now will be working

--- Exam/mid/car_sales/car_model/models.py
+++ Exam/mid/car_sales/car_model/models.py
@@ -8,7 +8,19 @@ class Car(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='car')
     image = models.ImageField(upload_to='car_model/media/uploads/', blank = True, null = True)
+    # image = models.ImageField(upload_to='uploads/', blank = True, null = True)
     # brand_name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+        
+    def __str__(self):
+        return f"Comments by {self.name}"
--- Exam/mid/car_sales/car_model/views.py
+++ Exam/mid/car_sales/car_model/views.py
@@ -8,13 +8,65 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView,UpdateView,DeleteView,DetailView
 
 # Create your views here.
-def add_car_model(request):
-    if request.method == 'POST':
-        car_form = forms.CarForm(request.POST)
-        if car_form.is_valid():
-            car_form.save()
-            messages.success(request, 'Added Car Model Successfully')
-            return redirect('add_car')
-    else:
-        car_form = forms.CarForm()
-    return render(request, 'add_car.html', {'form' : car_form})
+# def add_car_model(request):
+#     if request.method == 'POST':
+#         car_form = forms.CarForm(request.POST)
+#         if car_form.is_valid():
+#             car_form.save()
+#             messages.success(request, 'Added Car Model Successfully')
+#             return redirect('add_car')
+#     else:
+#         car_form = forms.CarForm()
+#     return render(request, 'add_car.html', {'form' : car_form})
+
+@method_decorator(login_required, name='dispatch')
+class AddPostCreateView(CreateView):
+    model = models.Car
+    form_class = forms.CarForm
+    template_name = 'add_car.html'
+    success_url = reverse_lazy('add_car')
+    def form_valid(self, form):
+        messages.success(self.request, 'Added Car Model Successfully')
+        return super().form_valid(form)
+
+
+@method_decorator(login_required, name='dispatch')
+class EditPostView(UpdateView):
+    model = models.Car
+    form_class = forms.CarForm
+    template_name = 'add_car.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('homepage')
+
+
+@method_decorator(login_required, name='dispatch')
+class DeletePostView(DeleteView):
+    model = models.Car
+    template_name = 'delete.html'
+    success_url = reverse_lazy('homepage')
+    pk_url_kwarg = 'id'
+
+
+class DetailPostView(DetailView):
+    model = models.Car
+    pk_url_kwarg = 'id'
+    template_name = 'car_details.html'
+
+    def post(self, request, *args, **kwargs):
+        comment_form = forms.CommentForm(data=self.request.POST)
+        post = self.get_object()
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+        return self.get(request, *args, **kwargs)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = self.object # post model er object ekhane store korlam
+        comments = post.comments.all()
+        comment_form = forms.CommentForm()
+        
+        context['comments'] = comments
+        context['comment_form'] = comment_form
+        return context
